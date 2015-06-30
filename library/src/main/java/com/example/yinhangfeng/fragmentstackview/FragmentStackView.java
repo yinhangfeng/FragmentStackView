aaf257385e71b3fcde2100182ce32bb597c8c691
yinhangfeng <darkwander.y@gmail.com>
2015-06-30 19:31:37 +0800

    xxx

--- library/src/main/java/com/example/yinhangfeng/fragmentstackview/FragmentStackView.java
+++ library/src/main/java/com/example/yinhangfeng/fragmentstackview/FragmentStackView.java
@@ -453,7 +453,6 @@ public class FragmentStackView extends ViewGroup {
         setMeasuredDimension(widthSize, heightSize);
 
         //测量子View
-        int foundDrawers = 0;
         final int childCount = getChildCount();
         for(int i = 0; i < childCount; i++) {
             final View child = getChildAt(i);
